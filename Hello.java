5cc6e1bc41e6d8ccd493509b236432a60339dd71
jiaminsun <1217594215@qq.com>
2017-10-09 21:19:05 +0800

    增加Show（）方法
    增加了Show方法

--- Hello.java
+++ Hello.java
@@ -2,4 +2,10 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello world");
 	}
+	
+	
+	public void show(){
+		System.out.println("show() method");
+	}
+		
 }
